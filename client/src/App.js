6229438e1c48bc616d7a00db823516d4e3f75c58
JohnCrossRollOn <juanrolon54@outlook.com>
2022-11-08 11:51:23 -0300

    Added Footer component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Chat from './Components/Chat'
-import Home from './Components/Home'
-import NavBar from './Components/NavBar'
-import { createTheme, ThemeProvider } from '@material-ui/core'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chat from "./Components/Chat";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import Footer from "./Components/Footer";
 
 function App() {
   const theme = createTheme({
     palette: {
       secondary: {
-        main: '#000'
-      }
-    }
-  })
+        main: "#000",
+      },
+    },
+  });
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/bienvenida' element={<Chat />} />
-        </Routes>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bienvenida" element={<Chat />} />
+          </Routes>
+          <Footer />
+        </div>
       </BrowserRouter>
     </ThemeProvider>
   );
