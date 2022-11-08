7a885e6942740a0e7588aa4b87602aaf94f8421f
AgustinMillan <victor.agustin.millan@gmail.com>
2022-11-08 17:07:00 -0300

    query ruta get PLANTS

--- api/src/routes/plants.js
+++ api/src/routes/plants.js
@@ -11,28 +11,40 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   const tabla = await Plants.findAll();
+  const { name } = req.query;
+
   try {
-    if (!tabla.length) {
-      for (let i = 0; i < dbBuild.length; i++) {
-        let nObj = {
-          namePlant: dbBuild[i].NOMBRE,
-          descripPlant: dbBuild[i].DESCRIPCION,
-          ubication: dbBuild[i].UBICACION,
-          luminosidad: dbBuild[i].LUMINOSIDAD,
-          riego: dbBuild[i].RIEGO,
-          tamano: dbBuild[i].TAMANIO,
-          tipo: dbBuild[i].TIPO,
-          clima: dbBuild[i].PREFERENCIA_CLIMATICA,
-          toxicidad: dbBuild[i].TOXICIDAD,
-          statePlant: true,
-          imagePlant: dbBuild[i].IMAGEN
-        };
-        await Plants.create(nObj);
-      }
-      const tabla2 = await Plants.findAll();
-      return res.status(201).send(tabla2);
+    if (name) {
+      const tabl2 = await Plants.findAll();
+
+      let newTable = tabl2.filter((e) =>
+        e.namePlant.toLocaleLowerCase().includes(name.toLocaleLowerCase())
+      );
+
+      res.status(200).send(newTable);
     } else {
-      return res.status(200).send(tabla);
+      if (!tabla.length) {
+        for (let i = 0; i < dbBuild.length; i++) {
+          let nObj = {
+            namePlant: dbBuild[i].NOMBRE,
+            descripPlant: dbBuild[i].DESCRIPCION,
+            ubication: dbBuild[i].UBICACION,
+            luminosidad: dbBuild[i].LUMINOSIDAD,
+            riego: dbBuild[i].RIEGO,
+            tamano: dbBuild[i].TAMANIO,
+            tipo: dbBuild[i].TIPO,
+            clima: dbBuild[i].PREFERENCIA_CLIMATICA,
+            toxicidad: dbBuild[i].TOXICIDAD,
+            statePlant: true,
+            imagePlant: dbBuild[i].IMAGEN,
+          };
+          await Plants.create(nObj);
+        }
+        const tabla2 = await Plants.findAll();
+        return res.status(201).send(tabla2);
+      } else {
+        return res.status(200).send(tabla);
+      }
     }
   } catch (error) {
     return res.status(400).send("algo salio mal");
