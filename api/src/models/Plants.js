e9644fb44d3312c8e70ba17bd2e3556e85bd004e
Calu Marquez <carlamarquez.distextil@gmail.com>
2022-11-10 14:34:40 -0300

    DataBase actualizada CM

--- api/src/models/Plants.js
+++ api/src/models/Plants.js
@@ -11,7 +11,7 @@ module.exports = (sequelize) => {
       unique: true
     },
     namePlant:{
-      type:DataTypes.ARRAY,
+      type:DataTypes.STRING,
       allowNull: false,
       unique:true,
     },
@@ -20,27 +20,27 @@ module.exports = (sequelize) => {
       allowNull: false
     },
     ubication:{
-      type: DataTypes.ARRAY,
+      type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false
     },
     ligth:{
-        type: DataTypes.ARRAY,
+        type: DataTypes.ARRAY(DataTypes.STRING),
         allowNull: false
     },
     whater:{
-        type: DataTypes.ARRAY,
+        type: DataTypes.ARRAY(DataTypes.STRING),
         allowNull: false
     },
     size:{
-        type: DataTypes.ARRAY,
+        type: DataTypes.ARRAY(DataTypes.STRING),
         allowNull: false
     },
     type:{
-        type: DataTypes.ARRAY,
+        type: DataTypes.ARRAY(DataTypes.STRING),
         allowNull: false
     },  
     climate:{
-        type: DataTypes.ARRAY,
+        type: DataTypes.ARRAY(DataTypes.STRING),
         allowNull: false
       }, 
       toxicity:{
