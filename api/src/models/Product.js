095e604b23075bff517709e08b236509db85f728
JohnCrossRollOn <juanrolon54@outlook.com>
2022-11-15 23:58:24 -0300

    improved and fixed some bugs in the api

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,57 +1,63 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Product', {
-    codProd: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    nameProd:{
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique:true,
-    },
-    descripProd:{
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    codCategory:{
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    starts:{
+  sequelize.define(
+    "Product",
+    {
+      codProd: {
         type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    price:{
-        type: DataTypes.DECIMAL,
-        allowNull: false
-    },
-    actualStock:{
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    minStock:{
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },  
-    maxStock:{
-        type: DataTypes.INTEGER,
-        allowNull: false
+        autoIncrement: true,
+        primaryKey: true,
       },
-    imageProd:{
+      nameProd: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      descripProd: {
         type: DataTypes.TEXT,
-        allowNull: true
-    }, 
-    stateProd:{
+        defaultVale: "Un producto.",
+      },
+      codCategory: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stars: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      currentStock: {
+        type: DataTypes.INTEGER,
+        defaultVale: 0,
+      },
+      minStock: {
+        type: DataTypes.INTEGER,
+        defaultVale: 0,
+      },
+      maxStock: {
+        type: DataTypes.INTEGER,
+        defaultVale: 9999,
+      },
+      imageProd: {
+        type: DataTypes.TEXT,
+        defaultVale:
+          "https://th.bing.com/th/id/R.8e9d9c4e9893628dcba520060fd726f5?rik=EeURkoXNo2Q1NQ&riu=http%3a%2f%2fproductonatural.co%2fwp-content%2fuploads%2f2020%2f01%2fProductos-varios-optimizada.png&ehk=dXE7Oc4SNtzf00%2bpTAlJ9dn3wi%2fpB%2fczM5Xdcwq06Vo%3d&risl=&pid=ImgRaw&r=0",
+      },
+      stateProd: {
         type: DataTypes.BOOLEAN,
-        allowNull: true
+        defaultVale: true,
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: false,
     }
-  },{
-    freezeTableName: true,
-    timestamps: false
-  });
+  );
 };
