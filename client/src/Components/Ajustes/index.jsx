8b64029e0721a2a8725678fd487a250ee77cc63a
unknown <leandro.florentin@hotmail.com>
2022-11-19 16:58:35 -0300

    focus

--- client/src/Components/Ajustes/index.jsx
+++ client/src/Components/Ajustes/index.jsx
@@ -10,23 +10,35 @@ import UsuariosInfo from '../UsuariosInfo/UsuarioInfo'
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 
 const Ajustes = () => {
-    const [selectedTab, setSelectedTab] = React.useState("Profile");
+  const [selectedTab, setSelectedTab] = React.useState("Profile");
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setSelectedTab(newValue);
   };
-    return (
-        <TabContext value={selectedTab} defaultValue={Profile}>
-        <TabList onChange={handleChange} aria-label="icon label tabs example">
-      <Tab icon={<BadgeIcon />} label="Perfil" value="Profile"/>
-      <Tab icon={<ManageAccountsIcon />} label="Admin Usuarios" value="AdminUser" />
-      <Tab icon={<AttachMoneyIcon />} label="DashBoard" value="DashBoard"/>
-    </TabList>
-      <TabPanel value="Profile"><Profile/></TabPanel>
-      <TabPanel value="AdminUser"><UsuariosInfo/></TabPanel>
-      <TabPanel value="DashBoard"><DashboardAdmin/></TabPanel>
-     </TabContext>  
-    )
+  return (
+    <TabContext value={selectedTab} defaultValue={Profile}>
+      <TabList onChange={handleChange} aria-label="icon label tabs example">
+        <Tab icon={<BadgeIcon />} sx={{
+          "&:focus": {
+            outline: "none"
+          }
+        }} label="Perfil" value="Profile" />
+        <Tab icon={<ManageAccountsIcon />} sx={{
+          "&:focus": {
+            outline: "none"
+          }
+        }} label="Admin Usuarios" value="AdminUser" />
+        <Tab icon={<AttachMoneyIcon />} sx={{
+          "&:focus": {
+            outline: "none"
+          }
+        }} label="DashBoard" value="DashBoard" />
+      </TabList>
+      <TabPanel value="Profile"><Profile /></TabPanel>
+      <TabPanel value="AdminUser"><UsuariosInfo /></TabPanel>
+      <TabPanel value="DashBoard"><DashboardAdmin /></TabPanel>
+    </TabContext>
+  )
 }
 
 export default Ajustes;
