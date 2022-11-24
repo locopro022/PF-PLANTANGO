b7a2ef8b4fef46fa78c6e3d690a396f4c0df6c6a
JohnCrossRollOn <juanrolon54@outlook.com>
2022-11-24 14:37:30 -0300

    added nosotros

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import {
   getTiposHuerta,
   getUser,
   carritoStorage,
-  traerNotificaciones
+  traerNotificaciones,
 } from "./redux/actions";
 import Detalle from "./Components/Detalle";
 import Breadcrumbs from "./Components/Breadcrumbs";
@@ -25,11 +25,12 @@ import Favoritos from "./Components/Favoritos";
 import { useAuth0 } from "@auth0/auth0-react";
 import UsuariosInfo from "./Components/UsuariosInfo/UsuarioInfo";
 //import PagoStripe from "./Components/PasarelaPago/Stripe";
-import DetailVivero from './Components/DetailVivero'
-import Vivero from './Components/Vivero'
-import Diario from "./Components/Diario/Index.jsx"
+import DetailVivero from "./Components/DetailVivero";
+import Vivero from "./Components/Vivero";
+import Diario from "./Components/Diario/Index.jsx";
 import SuccessPago from "./Components/PasarelaPago/SuccessPago";
 import CancelPago from "./Components/PasarelaPago/CancelPago";
+import Nosotros from "./Components/Nosotros";
 
 function App() {
   const dispatch = useDispatch();
@@ -49,10 +50,12 @@ function App() {
     if (user) {
       dispatch(getUser(user.email));
     }
-    dispatch(carritoStorage(JSON.parse(localStorage.getItem("carrito"))))
-    dispatch(traerNotificaciones(JSON.parse(localStorage.getItem("Notificaciones"))))
+    dispatch(carritoStorage(JSON.parse(localStorage.getItem("carrito"))));
+    dispatch(
+      traerNotificaciones(JSON.parse(localStorage.getItem("Notificaciones")))
+    );
   }, [user]);
-  console.log(user)
+  console.log(user);
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
@@ -78,6 +81,7 @@ function App() {
             <Route path="/recordatorio" element={<Diario />} />
             <Route path="/success" element={<SuccessPago />} />
             <Route path="/cancel" element={<CancelPago />} />
+            <Route path="/nosotros" element={<Nosotros />} />
           </Routes>
           <Footer />
         </div>
