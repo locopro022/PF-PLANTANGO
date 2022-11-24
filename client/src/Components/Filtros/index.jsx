b66f4bd1a1870f9b8a2853c75eae5bcba21dabd6
unknown <leandro.florentin@hotmail.com>
2022-11-24 19:54:56 -0300

    huertaCss

--- client/src/Components/Filtros/index.jsx
+++ client/src/Components/Filtros/index.jsx
@@ -2,12 +2,14 @@ import "./Filtros.css";
 
 const Filtros = (props) => {
   const cambio = (e) => {
+    console.log("CHECK", e.target.checked)
+    console.log("NAME", e.target.name)
     const selected = {
       type: e.target.name,
-      value: e.target.id,
-      checked: e.target.checked,
+      value: e.target.value,
+      checked: true,
     };
-
+    console.log("SELECT", selected)
     props.apply(selected);
   };
   const borrar = (e) => {
@@ -17,44 +19,41 @@ const Filtros = (props) => {
 
   return (
     <div>
-      <div className="container">
+      <div className="containerFiltrosHuer">
         <form
           onSubmit={borrar}
-          className="container-fluid altoCard"
+          className="altoCard"
           style={{
             display: "flex",
-            flexDirection: "column",
+            flexDirection: "row",
           }}
         >
           <h3 className=" text-left filtroText">Filtros</h3>
+          {props.filtros?.map &&
+            props.filtros.map(({ filter, options }, index) => (
+              <div className="container-fluid mb-2" key={index}>
+                <h5>{filter}</h5>
+                <select onChange={cambio} name={filter}>
+                  {options.map &&
+                    options.map(({ value, checked }, index) => (
+                      <option
+                        type="checkbox"
+                        className="form-check-input"
+                        checked={checked}
+                        value={value}
+                      >
+                        {value}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            ))}
           <button
             type="submit"
             className="btn btn-outline-danger d-inline mb-3"
           >
             Borrar Filtros
           </button>
-          {props.filtros?.map &&
-            props.filtros.map(({ filter, options }, index) => (
-              <div className="container-fluid mb-2" key={index}>
-                <h5>{filter}</h5>
-                {options.map &&
-                  options.map(({ value, checked }, index) => (
-                    <div className="form-group form-check ml-4" key={index}>
-                      <input
-                        type="checkbox"
-                        className="form-check-input"
-                        onChange={cambio}
-                        checked={checked}
-                        name={filter}
-                        id={value}
-                      />
-                      <label className="form-check-label" htmlFor={value}>
-                        {value}
-                      </label>
-                    </div>
-                  ))}
-              </div>
-            ))}
         </form>
       </div>
     </div>
