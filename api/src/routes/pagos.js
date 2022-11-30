533edfa5b5013bdac18b9bf45ce6cebbe210250c
unknown <leandro.florentin@hotmail.com>
2022-11-30 14:50:44 -0300

    cambioCors

--- api/src/routes/pagos.js
+++ api/src/routes/pagos.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const pagos = express();
+const cors = require("cors");
+pagos.use(
+  cors({
+    origin: ["http://localhost:3001", "https://checkout.stripe.com", "https://plantango.vercel.app"],
+  })
+);
 // const stripe = require('stripe')('sk_test_51M4r6KE0RaxicoaffBhIbFPuHCOEvqzNzmxo4RgTseEytjGAOckS3kAl1j3OZDDMIhhQNNxLqXPiUuUj16XLtQzS00sYj5nGq1')
 
 const Stripe = require("stripe")
