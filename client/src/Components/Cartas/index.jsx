7f0148de2db669d97d68c68bd86a3c4b5d15850e
AgustinMillan <victor.agustin.millan@gmail.com>
2022-11-30 11:57:16 -0300

    Soluicion fav 2.0

--- client/src/Components/Cartas/index.jsx
+++ client/src/Components/Cartas/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import Notiflix from 'notiflix'
+import Notiflix from "notiflix";
 import "./index.css";
 import { addFav, deleteFav, editPlantforLike } from "../../redux/actions";
 
@@ -17,33 +17,39 @@ const Cartas = (props) => {
     }
     if (user.idUser) {
       if (e.target.className === "favOFF") {
-        dispatch(addFav(user.idUser, item.id));
-        Notiflix.Notify.success('Agregada con exito.', {
-          zindex: 9999999
-        })
-        dispatch(
-          editPlantforLike({
-            codPlant: item.id,
-            namePlant: item.nombre,
-            descripPlant: item.descripcion,
-            tipo: item.caracteristica,
-            imagePlant: item.img,
-            likes: item.likes + 1,
-          })
-        ).then(props.aux("like"));
-        clsN = "favON"
+        dispatch(addFav(user.idUser, item.id))
+          .then(props.aux("like"))
+          .then(
+            dispatch(
+              editPlantforLike({
+                codPlant: item.id,
+                namePlant: item.nombre,
+                descripPlant: item.descripcion,
+                tipo: item.caracteristica,
+                imagePlant: item.img,
+                likes: item.likes + 1,
+              })
+            )
+          );
+        Notiflix.Notify.success("Agregada con exito.", {
+          zindex: 9999999,
+        });
+        clsN = "favON";
       } else if (e.target.className === "favON") {
-        dispatch(deleteFav(user.idUser, item.id));
-        dispatch(
-          editPlantforLike({
-            codPlant: item.id,
-            namePlant: item.nombre,
-            descripPlant: item.descripcion,
-            tipo: item.caracteristica,
-            imagePlant: item.img,
-            likes: item.likes - 1,
-          })
-        ).then(props.aux("like"))
+        dispatch(deleteFav(user.idUser, item.id))
+          .then(
+            dispatch(
+              editPlantforLike({
+                codPlant: item.id,
+                namePlant: item.nombre,
+                descripPlant: item.descripcion,
+                tipo: item.caracteristica,
+                imagePlant: item.img,
+                likes: item.likes - 1,
+              })
+            )
+          )
+          .then(props.aux("like"));
         clsN = "favOFF";
       }
     }
@@ -52,71 +58,66 @@ const Cartas = (props) => {
   function onOf(item, favorites) {
     if (favorites) {
       if (favorites.find((e) => e.codPlant === item.id)) {
-        return clsN = "favON";
+        return (clsN = "favON");
       } else {
-        return clsN = "favOFF";
+        return (clsN = "favOFF");
       }
     }
   }
   return (
     <div className="contenedorCartasFavoritos">
-      {
-        props.items?.length ?
-          <div className="cartas">
-            {props.items?.map &&
-              props.items.map((item, i) => (
-                <div className="cartaHuer">
-                  {item.img && (
-                    <Link
-                      key={i}
-                      to={`/huerta/${item.id}`}
-                      style={{ textDecoration: "none", color: "black" }}
-                    >
-                      <img
-                        src={item.img}
-                        alt="Un producto"
-                        loading="lazy"
-                        className="imagen cabeza"
-                      />
-                    </Link>
-                  )}
-                  <div className="cuerpo">
-                    <div>
-                      {item.nombre && (
-                        <h3 className="cuerpoTitulo">{item.nombre}</h3>
-                      )}
-                      {item.caracteristica && (
-                        <p className="cuerpoCaracteristica">
-                          {item.caracteristica.map((caracteristica, i) => (
-                            <span
-                              className="cuerpoCaracteristicaCaracteristica"
-                              key={i}
-                            >
-                              {caracteristica}
-                            </span>
-                          ))}
-                        </p>
-                      )}
-                    </div>
-                    {item.precio && (
-                      <p className="cuerpo-precio">${item.precio / 100}</p>
-                    )}
-                    <p className="numeroP">{`Likes ${item.likes}`}</p>
-                    <button
-                      className={
-                        favorites.length ? onOf(item, favorites) : clsN
-                      }
-                      onClick={(e) => addfav(e, item, user)}
+      {props.items?.length ? (
+        <div className="cartas">
+          {props.items?.map &&
+            props.items.map((item, i) => (
+              <div className="cartaHuer">
+                {item.img && (
+                  <Link
+                    key={i}
+                    to={`/huerta/${item.id}`}
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    <img
+                      src={item.img}
+                      alt="Un producto"
+                      loading="lazy"
+                      className="imagen cabeza"
                     />
+                  </Link>
+                )}
+                <div className="cuerpo">
+                  <div>
+                    {item.nombre && (
+                      <h3 className="cuerpoTitulo">{item.nombre}</h3>
+                    )}
+                    {item.caracteristica && (
+                      <p className="cuerpoCaracteristica">
+                        {item.caracteristica.map((caracteristica, i) => (
+                          <span
+                            className="cuerpoCaracteristicaCaracteristica"
+                            key={i}
+                          >
+                            {caracteristica}
+                          </span>
+                        ))}
+                      </p>
+                    )}
                   </div>
+                  {item.precio && (
+                    <p className="cuerpo-precio">${item.precio / 100}</p>
+                  )}
+                  <p className="numeroP">{`Likes ${item.likes}`}</p>
+                  <button
+                    className={favorites.length ? onOf(item, favorites) : clsN}
+                    onClick={(e) => addfav(e, item, user)}
+                  />
                 </div>
-              ))}
-          </div>
-          :
-          <div
-            className="containerNoHayNada"
-          >No se encontraron plantas</div>
-      }
+              </div>
+            ))}
+        </div>
+      ) : (
+        <div className="containerNoHayNada">No se encontraron plantas</div>
+      )}
     </div>
   );
 };
--- client/src/Components/Favoritos/Cartas.jsx
+++ client/src/Components/Favoritos/Cartas.jsx
@@ -50,17 +50,19 @@ const CartasFavoritos = ({ items }) => {
             </h4>
           )}
           {items.tipo && (
-              <p className="cuerpo-caracteristica">
-                {items.tipo.map((caracteristica, i) => (
-                  <span className="cuerpo-caracteristica-caracteristica" key={i}>
-                    {caracteristica}
-                  </span>
-                ))}
-              </p>
+            <p className="cuerpo-caracteristica">
+              {items.tipo.map((caracteristica, i) => (
+                <span className="cuerpo-caracteristica-caracteristica" key={i}>
+                  {caracteristica}
+                </span>
+              ))}
+            </p>
           )}
         </div>
         <div className="button-container">
-          {items.precio && <p className="cuerpo-precio">${items.precio / 100}</p>}
+          {items.precio && (
+            <p className="cuerpo-precio">${items.precio / 100}</p>
+          )}
           <button className="favON" onClick={(e) => eliminarFav(e, items)} />
         </div>
       </div>
