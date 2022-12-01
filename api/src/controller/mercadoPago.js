b8df2228fcaccbf263af119192702516acbcc848
unknown <leandro.florentin@hotmail.com>
2022-11-30 22:27:35 -0300

    ArregladoPasarela

--- api/src/controller/mercadoPago.js
+++ api/src/controller/mercadoPago.js
@@ -8,49 +8,49 @@ mercadopago.configure({
 
 const premiumController =
   ("/",
-  (req, res) => {
-    const {items} = req.body;
-    console.log(items);
-    let preference = {
-      // items: [
-      //   {
-      //     id: 123,
-      //     title: "Mi producto",
-      //     unit_price: 100,
-      //     quantity: 1,
-      //     currency_id: "ARS",
-      //   },
-      // ],
+    (req, res) => {
+      const { items } = req.body;
+      console.log(items);
+      let preference = {
+        // items: [
+        //   {
+        //     id: 123,
+        //     title: "Mi producto",
+        //     unit_price: 100,
+        //     quantity: 1,
+        //     currency_id: "ARS",
+        //   },
+        // ],
 
-      items: items.map((i) => ({
-        id: i.id,
-        title: i.title,
-        unit_price: parseInt(i.unit_price),
-        quantity: i.quantity,
-        currency_id: "ARS",
-      })),
+        items: items.map((i) => ({
+          id: i.id,
+          title: i.title,
+          unit_price: parseInt(i.unit_price),
+          quantity: i.quantity,
+          currency_id: "ARS",
+        })),
 
-      //notification_url: "https://misitio/api", //redirige despues de la compra
-      back_urls: {
-        failure: "http://localhost:3001/PaymentFail",
-        pending: "http://localhost:3001/PaymentFail",
-        success: "https://localhost:3001/PaymentOk",
-      },
-      auto_return: "approved",
-    };
+        //notification_url: "https://misitio/api", //redirige despues de la compra
+        back_urls: {
+          failure: "https://plantango.vercel.app/cancel",
+          pending: "https://plantango.vercel.app/cancel",
+          success: "https://plantango.vercel.app/success",
+        },
+        auto_return: "approved",
+      };
 
-    mercadopago.preferences
-      .create(preference)
-      .then(function (response) {
-        // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
+      mercadopago.preferences
+        .create(preference)
+        .then(function (response) {
+          // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
 
-        // console.log('AAAAAA', response.body.init_point);
-        res.json({url: response.body.init_point});
-      })
+          // console.log('AAAAAA', response.body.init_point);
+          res.json({ url: response.body.init_point });
+        })
 
-      .catch(function (error) {
-        console.log(error);
-      });
-  });
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
 
 module.exports = { premiumController };
